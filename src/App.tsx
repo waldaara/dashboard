29d8033bd48f826c877e1ebac15334a6939cfdc0
Cliente CONTROL-PC <CltControl@GGIECLUGWRK197.espol.edu.ec>
2024-11-13 10:02:37 -0500

    g12

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css'
 import Grid from '@mui/material/Grid2'
+import Indicator from './components/Indicator';
+import BasicTable from './components/BasicTable';
+import SimpleLineChart from "./components/SimpleLineChart"
 
 function App() {
 
@@ -7,16 +10,16 @@ function App() {
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 1'} subtitle={'Unidad 1'} value={1.23}/></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 2'} subtitle={'Unidad 2'} value={3.12}/></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 3'} subtitle={'Unidad 3'} value={2.31}/></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 4'} subtitle={'Unidad 4'} value={3.21}/></Grid>
 
         {/* Tabla */}
-        <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
+        <Grid size={{ xs: 12, xl: 8 }}><BasicTable/></Grid>
 
         {/* Gráfico */}
-        <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+        <Grid size={{ xs: 12, xl: 4 }}><SimpleLineChart/></Grid>
        
     </Grid>)
 }
