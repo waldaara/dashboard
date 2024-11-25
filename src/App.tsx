549ac07cc13c1fe487b25de7ebbadc1764b8cd87
David Aragundy <136662251+waldaara@users.noreply.github.com>
2024-11-25 10:40:11 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,50 @@
 import './App.css'
 import Grid from '@mui/material/Grid2'
-import Indicator from './components/IndicatorWeather';
+import IndicatorWeather from './components/IndicatorWeather';
 import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
 
 function App() {
 
-  return (
-    <Grid container spacing={5}>
+    return (
+        <Grid container spacing={5}>
 
-       {/* Indicadores  */}
-             <Grid size={ ... }>
-                 <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
-             </Grid>
+            {/* Indicadores  */}
+            <Grid size={{ xs: 12, xl: 3 }}>
+                <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+            </Grid>
 
-             <Grid size={ ... }>
-                 <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
-             </Grid>
-                
-             <Grid size={ ... }>
-                 <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
-             </Grid>
-                
-             <Grid size={ ... }>
-                 <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
-             </Grid>
+            <Grid size={{ xs: 12, xl: 3 }}>
+                <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+            </Grid >
 
-        {/* Tabla */}
-        <Grid size={{ xs: 12, xl: 8 }}> 
-          {/* Grid Anidado */}
-                 <Grid container spacing={2}>
-                     <Grid size={{ xs: 12, xl: 3 }}>
-                         <ControlWeather/>
-                     </Grid>
-                     <Grid size={{ xs: 12, xl: 9 }}>
-                         <TableWeather/>
-                     </Grid>
-                 </Grid>
-        </Grid>
+            <Grid size={{ xs: 12, xl: 3 }}>
+                <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+            </Grid>
 
-        {/* Gráfico */}
-             <Grid size={ ... }>
-                 <LineChartWeather/>
-             </Grid>
-       
-    </Grid>)
+            <Grid size={{ xs: 12, xl: 3 }}>
+                <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+            </Grid>
+
+            {/* Tabla */}
+            <Grid size={{ xs: 12, xl: 8 }}>
+                {/* Grid Anidado */}
+                <Grid container spacing={2}>
+                    <Grid size={{ xs: 12, xl: 3 }}>
+                        <ControlWeather />
+                    </Grid>
+                    <Grid size={{ xs: 12, xl: 9 }}>
+                        <TableWeather />
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            {/* Gráfico */}
+            <Grid size={{ xs: 12, xl: 4 }}>
+                <LineChartWeather />
+            </Grid>
+        </Grid >)
 }
 
 export default App
