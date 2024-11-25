5f2e735dcbc3a2a829a70160aa08a80cc952df99
David Aragundy <136662251+waldaara@users.noreply.github.com>
2024-11-25 10:15:33 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Grid from '@mui/material/Grid2'
 import Indicator from './components/IndicatorWeather';
 import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
-import SimpleLineChart from "./components/SimpleLineChart"
+import LineChartWeather from './components/LineChartWeather';
 
 function App() {
 
@@ -41,7 +41,9 @@ function App() {
         </Grid>
 
         {/* Gráfico */}
-        <Grid size={{ xs: 12, xl: 4 }}><SimpleLineChart/></Grid>
+             <Grid size={ ... }>
+                 <LineChartWeather/>
+             </Grid>
        
     </Grid>)
 }
