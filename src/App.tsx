a679c1735bd91c4653c1a53a2e852a7c45f4c8ed
David Aragundy <136662251+waldaara@users.noreply.github.com>
2024-11-25 10:14:09 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
 import Grid from '@mui/material/Grid2'
-import Indicator from './components/Indicator';
-import BasicTable from './components/BasicTable';
+import Indicator from './components/IndicatorWeather';
+import TableWeather from './components/TableWeather';
+import ControlWeather from './components/ControlWeather';
 import SimpleLineChart from "./components/SimpleLineChart"
 
 function App() {
@@ -9,14 +10,35 @@ function App() {
   return (
     <Grid container spacing={5}>
 
-        {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 1'} subtitle={'Unidad 1'} value={1.23}/></Grid>
-        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 2'} subtitle={'Unidad 2'} value={3.12}/></Grid>
-        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 3'} subtitle={'Unidad 3'} value={2.31}/></Grid>
-        <Grid size={{ xs: 12, xl: 3 }}><Indicator  title={'Indicador 4'} subtitle={'Unidad 4'} value={3.21}/></Grid>
+       {/* Indicadores  */}
+             <Grid size={ ... }>
+                 <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
+             </Grid>
+
+             <Grid size={ ... }>
+                 <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+             </Grid>
+                
+             <Grid size={ ... }>
+                 <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+             </Grid>
+                
+             <Grid size={ ... }>
+                 <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+             </Grid>
 
         {/* Tabla */}
-        <Grid size={{ xs: 12, xl: 8 }}><BasicTable/></Grid>
+        <Grid size={{ xs: 12, xl: 8 }}> 
+          {/* Grid Anidado */}
+                 <Grid container spacing={2}>
+                     <Grid size={{ xs: 12, xl: 3 }}>
+                         <ControlWeather/>
+                     </Grid>
+                     <Grid size={{ xs: 12, xl: 9 }}>
+                         <TableWeather/>
+                     </Grid>
+                 </Grid>
+        </Grid>
 
         {/* Gráfico */}
         <Grid size={{ xs: 12, xl: 4 }}><SimpleLineChart/></Grid>
